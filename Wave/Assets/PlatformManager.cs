067c5777a7719e10067b2af76ca985f31178d320
Edgar JRG <erodriguez@teravisiongames.com>
2017-01-21 10:17:19 -0300

    proyect

---
+++ Wave/Assets/PlatformManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour {
+
+	public List<GameObject> platforms;
+	// Use this for initialization
+	void Start () {
+		this.platforms = new List<GameObject>();
+
+		foreach (Transform platform in this.gameObject.transform)
+		{
+			this.platforms.Add(platform.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Wave/Assets/Scripts/Manager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+    public static Manager gManager;
+    public PlayerController player;
+	// Use this for initialization
+	void Awake () {
+        gManager = this;
+	}
+
+}
